8d24d69c6f7e909d8175246defe00342b80e200e
cevatarmutlu <cevatarmutlu@gmail.com>
2024-05-05 16:11:37 +0300

    update

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ if __name__ == '__main__':
   ### READ EXCEL ###
 
   ### NEO4j ###
-  my_neo4j = MyNeo4j("bolt://localhost:7687", "neo4j", "test1234?_")
+  neo4j_conn = MyNeo4j("bolt://localhost:7687", "neo4j", "test1234?_")
 
   for _, row in assets.iterrows():
-    my_neo4j.create_asset_node(
+    neo4j_conn.create_asset_node(
       asset_name=row['AssetName'],
       asset_type=row['AssetType'],
       asset_cluster=row['AssetCluster']
@@ -22,25 +22,38 @@ if __name__ == '__main__':
   ### NEO4j ###
 
   ### SECTION ###
-  new_needs = pd.merge(needs, assets, how='left', left_on='asset_name', right_on='AssetName')
-  new_offers = pd.merge(offers, assets, how='left', left_on='asset_name', right_on='AssetName')
+  needs_assets = pd.merge(needs, assets, how='left', left_on='asset_name', right_on='AssetName') \
+    .rename(columns= {
+      'group_id': 'need_group_id', 
+      'AssetName': 'need_asset_name', 
+      'AssetType': 'need_asset_type',
+      'AssetCluster': 'need_asset_cluster'
+    }).drop(columns='asset_name')
+  
+  offers_assets = pd.merge(offers, assets, how='left', left_on='asset_name', right_on='AssetName') \
+    .rename(columns= {
+      'group_id': 'offer_group_id', 
+      'AssetName': 'offer_asset_name', 
+      'AssetType': 'offer_asset_type',
+      'AssetCluster': 'offer_asset_cluster'
+    }).drop(columns='asset_name')
   ### SECTION ###
 
   ### FIND MATHING ASSETS ####
-  df = pd.merge(new_needs, new_offers, how='inner', left_on='need_value', right_on='offer_value')
-  df = df.loc[df['group_id_x'] + 1 == df['group_id_y']][['AssetName_x', 'AssetType_x', 'AssetCluster_x', 'AssetName_y', 'AssetType_y', 'AssetCluster_y']].drop_duplicates(ignore_index=True)
+  df = pd.merge(needs_assets, offers_assets, how='inner', left_on='need_value', right_on='offer_value')
+  df = df.loc[df['need_group_id'] + 1 == df['offer_group_id']][['need_asset_name', 'need_asset_type', 'need_asset_cluster', 'offer_asset_name', 'offer_asset_type', 'offer_asset_cluster']].drop_duplicates(ignore_index=True)
   ### FIND MATHING ASSETS ####
 
   # CREATE RELATIONSHIPS BETWEEN ASSET NODES
   for _, row in df.iterrows():
-    my_neo4j.create_asset_relation(
-      needs_asset_name=row['AssetName_x'],
-      needs_asset_type=row['AssetType_x'],
-      needs_asset_cluster=row['AssetCluster_x'],
-      offers_asset_name=row['AssetName_y'],
-      offers_asset_type=row['AssetType_y'],
-      offers_asset_cluster=row['AssetCluster_y']
+    neo4j_conn.create_asset_relation(
+      needs_asset_name=row['need_asset_name'],
+      needs_asset_type=row['need_asset_type'],
+      needs_asset_cluster=row['need_asset_cluster'],
+      offers_asset_name=row['offer_asset_name'],
+      offers_asset_type=row['offer_asset_type'],
+      offers_asset_cluster=row['offer_asset_cluster']
     )
 
   # CLOSE CONNECTION
-  my_neo4j.close()
+  neo4j_conn.close()
